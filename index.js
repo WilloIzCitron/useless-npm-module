7ba958e142a59d15c99b29b9822e40d30db207c5
Tegas Aziz <tegasaziz897@gmail.com>
2021-01-12 03:29:18 +0000

    update

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ exports.textgen = function(query, ctx) {
     if(query == "base64"){
         return Buffer.from(ctx).toString('base64')
     }
+    if(query == "ascii"){
+        return Buffer.from(ctx).toString('ascii')
+    }
+    if(query == "binary"){
+        return Buffer.from(ctx).toString('binary')
+    }
     if(query == "flipped"){
         return fliptext(ctx)
     }else{
